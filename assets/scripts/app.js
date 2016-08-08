2eb6f981a4c318bed368c234b45e7d4608fb486d
Caio Barrocal <caiobarrocal@MacBook-Pro-de-Caio.local>
2016-08-08 20:41:36 -0300

    Interações

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,33 @@
+var getTask = function(x) {
+
+	var task = document.getElementById(x).value;
+	return task;
+}
+
+var removeTask = function(x) {
+
+	$(x).remove();
+}
+
 var main = function() {
-    
+
+    $('.task-adder').focus(function() {
+    	$(this).keyup(function(event) {
+    		if (event.which === 13 ) {
+    			var newTask = getTask('inputter');
+    			$('.list').append('<div class="task newDiv" style="display:none;"><div class="checkbox"></div><p>'+newTask.toString()+'</p></div>');
+    			$('.newDiv').appendTo('.list').show(100);
+
+    			var event;
+    		};
+
+    	});
+    	 
+    });
+
     $('.checkbox').click(function() {
         $(this).addClass('checked');
+        $(this).parent().hide(400);
     });
 };
 
