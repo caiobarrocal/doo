3bfe3c3d8a191d3ec732e107d46219b6a4443c38
Caio Barrocal <caiobarrocal@MacBook-Pro-de-Caio.local>
2016-08-09 20:16:49 -0300

    Interations corrected and add list implementing

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,14 +6,43 @@ var getTask = function(x) {
 
 var main = function() {
 
+
+    //side-menu interation
+
+    $('.lists-menu').hover(function() {
+
+        $('.add-list').fadeIn(200);
+    }, function() {
+
+         $('.add-list').fadeOut(200);
+    }
+    );
+
+     $('.projects-menu').hover(function() {
+
+        $('.add-project').fadeIn(200);
+    }, function() {
+
+         $('.add-project').fadeOut(200);
+    }
+    );
+
+    $('.add-list').click(function() {
+
+        $('.new-flash').fadeIn(100);
+    })
+
+
+
+    // task adding and removing
+
     $('.task-adder').focus(function() {
     	$(this).keyup(function(event) {
-    		if (event.which === 13 ) {
+    		if (event.which === 13 && this.value !== '') {
     			var newTask = getTask('inputter');
     			$('.list').append('<div class="task newDiv" style="display:none;"><div class="checkbox"></div><p>'+newTask.toString()+'</p></div>');
-    			$('.newDiv').appendTo('.list').show(100);
-                
-    			var event;
+    			$('.newDiv').show(100);
+                this.value='';
     		};
 
     	});
@@ -23,9 +52,18 @@ var main = function() {
     $('.list').on( 'click','.task .checkbox', function() {
         $(this).addClass('checked');
         $(this).parent().fadeOut(200, function(){
-            $(this).remove();
+        $(this).remove();
         });
     });
+
+    
+
+   /* $('.add-object a').click(function() {
+
+        var opened = window.open("", "_self");
+        opened.document.write("<html><head><title>My title</title></head><body>test</body></html>");
+    })*/
+    
 };
 
 $(document).ready(main);
